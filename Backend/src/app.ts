1c49ce7e4feb3a24c006a76983eca1bf08571f85
kartikchoudhary7199 <kartikchoudhary7199@gmail.com>
2026-04-16 00:04:46 +0530

    Updated backend structure and AI services

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -1,10 +1,36 @@
-import express from 'express'
+import express from 'express';
+import runGraph from "./ai/graph.ai.js"
+import cors from "cors"
 
-const app = express()
+const app = express();
+app.use(express.json())
+app.use(cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+    credentials: true,
+}))
 
 
-app.get('/health', (req, res) => {
-    res.status(200).json({ status: 'OK' })
+app.get('/', async (req, res) => {
+
+    const result = await runGraph("Write an code for Factorial function in js")
+
+    res.json(result)
 })
 
-export default app
+app.post("/invoke", async (req, res) => {
+
+    const { input } = req.body
+    const result = await runGraph(input)
+
+    res.status(200).json({
+        message: "Graph executed successfully",
+        success: true,
+        result
+    })
+
+})
+
+
+
+export default app;
--- Backend/src/config/config.ts
+++ Backend/src/config/config.ts
@@ -1,18 +1,13 @@
-import { config } from "dotenv";
-config();
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 
-
-type CONFIG = {
-    readonly GOOGLE_API_KEY: string;
-    readonly MISTRAL_API_KEY: string;
-    readonly COHERE_API_KEY: string;
+const config = {
+    GOOGLE_API_KEY: process.env.GOOGLE_API_KEY || '',
+    MISTRALAI_API_KEY: process.env.MISTRAL_API_KEY || '',
+    COHERE_API_KEY: process.env.COHERE_API_KEY || '',
 }
 
-const config: CONFIG = {
-    GOOGLE_API_KEY: process.env.GOOGLE_API_KEY || "",
-    MISTRAL_API_KEY: process.env.MISTRAL_API_KEY || "",
-    COHERE_API_KEY: process.env.COHERE_API_KEY || "",
-}
 
 export default config;
